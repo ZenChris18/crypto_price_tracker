15479d149ab14c54159bffc7cbbde439cf367e22
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2025-07-14 20:21:12 +0800

    added search parameters for each coin

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from watchlist import watchlist
 from fetcher import get_price
-from usd_php import get_usd_to_php # this returns the conversion of 1ust to php
+from usd_php import get_usd_to_php # this returns the conversion of 1usd to php
 
 app = FastAPI()
 
@@ -10,6 +10,11 @@ def root():
     return {"message": "Crypto tracker API is running"}
 
 @app.get("/prices")
-def price():
-    return {coin: get_price(coin_id) for coin, coin_id in watchlist.items()}
+def price(coin_id: str="axs"):
+    # return {coin: get_price(coin_id) for coin, coin_id in watchlist.items()} this is for returning the whole watchlist
+    if coin_id in watchlist:
+        coin_name = watchlist[coin_id]
+        return {coin_id: round(get_price(coin_name) * get_usd_to_php(), 2)} 
+    else:
+        return {"Input Valid Coin name"}
 
